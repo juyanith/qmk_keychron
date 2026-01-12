8b2f6e2c17429121f55b4e027a1c7b0f8dcc1eed
Van Stonecypher <van@unnamed.net>
2026-01-12 16:54:41 -0600

    Changed caps lock to CAG for k2_he

--- keyboards/keychron/k2_he/ansi/keymaps/juyanith/keymap.c
+++ keyboards/keychron/k2_he/ansi/keymaps/juyanith/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, UG_VALD,  UG_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SNAP,  KC_DEL,   UG_NEXT,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        QK_LEAD,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+        KC_LCAG,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
         KC_LSFT,  MT_ZGUI,  MT_XALT,  MT_CSFT,  MT_VCTL,  KC_B,     KC_N,     MT_MCTL,  MT_CMAS,  MT_DOTA,  MT_SLSG,            KC_RSFT,            KC_UP,    KC_END,
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               SPC_NAV,                                KC_RCMMD, FN_MAC,   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
